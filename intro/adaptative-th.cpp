13c9b8376d65b66e7ee67cc12b9e8e2c8f77a151
ARRICAU Estelle <earricau@cemonum.pedago.ipb.fr>
2019-10-18 20:08:50 +0200

    update

--- intro/adaptative-th.cpp
+++ intro/adaptative-th.cpp
@@ -10,13 +10,16 @@ using namespace std;
 double mean(Mat block){
   Size s = block.size();
   int h = s.height;
+  cout<<h<<endl;
   int w = s.width;
-  int sum;
+  cout<<w<<endl;
+  int sum = 0;
   for (int i = 0; i < h; i++){
     for(int j = 0; j < w; j++){
       sum += block.at<uchar>(i,j);
     }
   }
+  cout<<sum / (h*w)<<endl;
   return sum / (h*w);
 }
 
@@ -27,40 +30,50 @@ process(const char* ims, int radius, int cst)
   Size s = image.size();
   int h = s.height;
   int w = s.width;
-  cout<<"h ="<<h<<endl;
-  cout<<"w ="<<w<<endl;
-  (void)cst;
   Mat imageMoy(h,w,CV_8UC1);
   for (int i = 0; i < h; i++){
     for(int j = 0; j < w; j++){
+      //cout<<"i= "<<i-radius<<endl;
+      //cout<<"j= "<<j-radius<<endl;
       Mat block;
       if(j+radius < w && i+radius < h && j-radius >=0 && i-radius >= 0){
-        cout<<"i-rad="<<i-radius<<endl;
-        cout<<"j-rad"<<j-radius<<endl;
+        //cout<<"i-rad= "<<i-radius<<endl;
+        //cout<<"j-rad "<<j-radius<<endl;
         block = image(Rect(j-radius,i-radius,2*radius+1,2*radius+1));
       }
       else{
-        cout<<"hello"<<endl;
         int deltaJ = 0;
         int deltaI = 0;
-        while(j + deltaJ < 0){
+        while(j - radius + deltaJ < 0){
+        //cout<<"j - radius + deltaJ "<<j - radius + deltaJ <<endl;
           deltaJ++;
         }
-        while(i + deltaI < 0){
+        while(i - radius + deltaI < 0){
           deltaI++;
+          //cout<<"i - radius + deltaI "<<i - radius + deltaI <<endl;
         }
-        while(j + deltaJ > w-1){
+        while(j + radius + deltaJ > w-1){
           deltaJ--;
+          //cout<<"j + radius + deltaJ "<<j - radius + deltaJ <<endl;
         }
-        while(i + deltaI > h-1){
+        while(i + radius + deltaI > h-1){
           deltaI--;
+          //cout<<"i + radius + deltaI "<<i - radius + deltaI <<endl;
         }
-        block = image(Rect(j-radius+deltaJ,i-radius+deltaI,2*radius+1-deltaJ,2*radius+1-deltaI));
+        block = image(Rect(j-radius+deltaJ,i-radius+deltaI,2*radius+1-abs(deltaJ),2*radius+1-abs(deltaI)));
       }
-      imageMoy.at<uchar>(i,j) = mean(block);
+      int val = mean(block) - cst;
+      if(imageMoy.at<uchar>(i,j) <= val)
+        imageMoy.at<uchar>(i,j) = 0;
+      else
+        imageMoy.at<uchar>(i,j) = 255;
       //cout<<mean(block)<<endl;
     }
   }
+  imshow("img moy",imageMoy);
+  imwrite("th.png",imageMoy);
+  cout<<"h ="<<h<<endl;
+  cout<<"w ="<<w<<endl;
 }
 
 void
